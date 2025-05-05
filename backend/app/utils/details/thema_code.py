f105d5d68f38babd094385ef5c7a5dc34e6d7d1f
HugoPuybareau <hugo.puybareau@bearingpoint.com>
2025-05-05 11:25:05 +0200

    thema code pipeline

--- backend/app/utils/details/thema_code.py
+++ backend/app/utils/details/thema_code.py
@@ -1,18 +1,29 @@
 # backend/app/utils/thema_code.py
 
-import json
-
-from backend.app.utils.details.llm import build_1st_letter_thema_code, build_2nd_letter_thema_code
-from backend.app.storage.storage import get_2nd_letter_thema_code_prompt, get_thema_code_desc
+from backend.app.utils.details.llm import build_1st_letter, build_next_letter
+from backend.app.storage.storage import (
+    get_2nd_letter_prompt,
+    get_3rd_letter_prompt,
+    get_thema_code_desc
+)
 
 def thema_code_pipeline(synopsis: str):
-    # first letter
-    first_letter = build_1st_letter_thema_code(synopsis)
-    # second letter
+    first_letter = build_1st_letter(synopsis)
     print(f"[THEMA_CODE] First letter: {first_letter}")
-    prompt_for_2nd_letter = get_2nd_letter_thema_code_prompt(first_letter)
-    primary_thema_code = build_2nd_letter_thema_code(synopsis, prompt_for_2nd_letter)
-    print(f"[THEMA_CODE] Primary thema code: {primary_thema_code}")
-    final = primary_thema_code + " : " + get_thema_code_desc(primary_thema_code)
-    print(f"[THEMA_CODE] Final thema code: {final}") 
-    return final
+    prompt_for_2nd_letter = get_2nd_letter_prompt(first_letter)
+    second_letter = build_next_letter(synopsis, prompt_for_2nd_letter)
+    print(f"[THEMA_CODE] Second letter: {second_letter}")
+    primary_output = second_letter + " : " + get_thema_code_desc(second_letter)
+    print(f"[THEMA_CODE] Primary output: {primary_output}")
+    prompt_for_3rd_letter = get_3rd_letter_prompt(second_letter)
+    third_letter = build_next_letter(synopsis, prompt_for_3rd_letter)
+    print(f"[THEMA_CODE] Third letters: {third_letter}")
+    code_values = [code.strip().upper() for code in third_letter.split(",")]
+    secondary_results = []
+    for code in code_values:
+            secondary_results.append({
+                "code": code,
+                "label": get_thema_code_desc(code)
+            })
+    print(f"[THEMA_CODE] Secondary results: {secondary_results}")
+    return primary_output, secondary_results
